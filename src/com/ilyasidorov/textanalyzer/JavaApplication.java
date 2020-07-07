bd30189d75b5c8a31393a9117e74ef626a79a75f
Ilya Sidorov <ilyasidorov114@gmail.com>
2020-07-07 11:08:40 +0300

    Handle IOException

--- src/com/ilyasidorov/textanalyzer/JavaApplication.java
+++ src/com/ilyasidorov/textanalyzer/JavaApplication.java
@@ -1,20 +1,24 @@
 package com.ilyasidorov.textanalyzer;
 
 import java.io.IOException;
+import java.nio.file.NoSuchFileException;
 
 public class JavaApplication {
 
     static String filePath = "E:\\projects\\text-analizer\\src\\com\\ilyasidorov\\textanalyzer\\file.txt";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         TextFromFileAnalyzer analyzer = new TextFromFileAnalyzer();
 
-        analyzer.analyze(filePath);
+        try {
+            analyzer.analyze(filePath);
+        } catch (NoSuchFileException e) {
+            System.out.println("Couldn't find your file, man. Check your path and try it again");
+        } catch (IOException e) {
+            System.out.println("Something wrong with IO");
+            e.printStackTrace();
+        }
 
     }
-
-
-
-
 
 }
